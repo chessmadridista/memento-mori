29413de4dc8c6fa32644beee41acb3ba180994f6
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-02-13 09:27:35 +0530

    Add input fields for birthdate and life expectancy. Add 'Update' button.

--- app/src/components/calendar/Calendar.js
+++ app/src/components/calendar/Calendar.js
@@ -8,10 +8,23 @@ years = years.map((year) => {
     );
 });
 
+function Form() {
+    return (
+        <form>
+            <label for="birthDate">Please select your birthdate</label>
+            <input type="date"></input>
+            <label for="lifeExpectancy">Please enter your life expectancy</label>
+            <input type="number" min="0" max="150"></input>
+            <button>Update</button>
+        </form>
+    );
+}
+
 function Calendar() {
     return (
         <>
             <h1>Memento Mori Calendar</h1>
+            <Form />
             { years }
         </>
     );
