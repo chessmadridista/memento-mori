6430afc4e3f3bd86639b501b9f1125bac5ac7b54
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-02-14 17:17:18 +0530

    Add animation to the app.

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './App.scss';
 import Calendar from './components/calendar/Calendar';
 import Form from './components/form/Form';
+import { useState } from 'react';
 
 function App() {
+  const [showForm, setShowForm] = useState(true);
+
   return (
     <>
       <h1>Memento Mori Calendar</h1>
-      <Form />
-      <Calendar />
+      { showForm ? <Form /> : <Calendar /> }
     </>
   );
 }
--- app/src/components/form/Form.js
+++ app/src/components/form/Form.js
@@ -1,17 +1,17 @@
-import styles from './Form.module.css';
+import styles from './Form.module.scss';
 
 function Form() {
     return (
         <form>
-            <div class={ styles['field__container'] }>
+            <div className={ styles['field__container'] }>
                 <label htmlFor="birthDate">Please select your birthdate</label>
                 <input type="date" id="birthDate"></input>
             </div>
-            <div class={ styles['field__container'] }>
+            <div className={ styles['field__container'] }>
                 <label htmlFor="lifeExpectancy">Please enter your life expectancy</label>
                 <input type="number" id="lifeExpectancy" min="0" max="150"></input>
             </div>
-            <button>Update</button>
+            <button className={ styles['btn__submit'] }>Update</button>
         </form>
     );
 }
