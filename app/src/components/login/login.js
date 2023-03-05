dad7d17c7f09d0b87d239b7c66be1d3b009f5e1a
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-03-05 13:37:40 +0530

    Add form details of login.

--- app/src/components/login/login.js
+++ app/src/components/login/login.js
@@ -1,11 +1,48 @@
 import styles from './login.module.css';
-import { Card } from 'antd';
+import { Card, Form, Input, Button } from 'antd';
 
 function Login() {
+    function onFinish() {
+        alert("Your form has been submitted.");
+    }
+
     return (
-        <Card className={ styles["card-login"] }>
-            Login page!
-        </Card>
+        <Card title="Login" className={ styles["card__login"] }>
+        <Form 
+        className={ styles.form }
+        layout="vertical"
+        onFinish={ onFinish }>
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={
+                    [
+                        {
+                            required: true,
+                            message: "Please enter your birth date.",
+                        },
+                    ]
+                }>
+                <Input type="text" className={ styles.input } addonAfter="years" placeholder="80" />
+            </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={
+                    [
+                        {
+                            required: true,
+                            message: "Please enter age at your death.",
+                        },
+                    ]
+                }>
+                <Input type="password" className={ styles.input } addonAfter="years" placeholder="80" />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" block>Login</Button>
+            </Form.Item>
+        </Form>
+    </Card>
     );
 }
 
