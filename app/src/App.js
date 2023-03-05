541b6f1cd9a301f3a02ddde0376d76320ca48a10
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-03-05 13:25:30 +0530

    Add basic files for login component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import styles from './App.module.scss';
 import FormContainer from './components/form/Form';
+import Login from './components/login/login';
 import Calendar from './components/calendar/Calendar';
 import Spinner from './components/auxialiary/spinner/Spinner';
 import {
@@ -16,6 +17,7 @@ function App() {
       { loading ? <Spinner className={ styles.loader } /> : null }
       <h1 className={ styles.heading }>Memento Mori Calendar</h1>
       <Routes>
+        <Route path="/login" element={ <Login /> }></Route>
         <Route path='/' element={ <FormContainer /> }></Route>
         <Route path='/calendar' element={ <Calendar /> }></Route>
       </Routes>
---
+++ app/src/components/login/login.js
@@ -0,0 +1,11 @@
+import styles from './login.module.css';
+
+function Login() {
+    return (
+        <div>
+            Login page!
+        </div>
+    );
+}
+
+export default Login;
